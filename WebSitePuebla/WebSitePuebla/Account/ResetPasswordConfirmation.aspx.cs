dafcb34ccffc8000d35b891810ca8f2b4037319e
alemart <alemart@DESKTOP-BJ71VOA>
2017-04-03 16:35:58 -0500

    Version 1 de la aplicación

---
+++ WebSitePuebla/WebSitePuebla/Account/ResetPasswordConfirmation.aspx.cs
@@ -0,0 +1,8 @@
+﻿using System.Web.UI;
+
+namespace WebSitePuebla.Account
+{
+    public partial class ResetPasswordConfirmation : Page
+    {
+    }
+}
---
+++ WebSitePuebla/WebSitePuebla/Startup.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(WebSitePuebla.Startup))]
+namespace WebSitePuebla
+{
+    public partial class Startup {
+        public void Configuration(IAppBuilder app) {
+            ConfigureAuth(app);
+        }
+    }
+}
